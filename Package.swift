a9177a5feb5da55319d95ee89d121b26cdb8049b
Helge Heß <devteam@zeezide.de>
2019-03-21 16:13:19 +0100

    KituraCORS, not Kitura-CORS
    ... is 'da module name.

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
                     "CloudEnvironment",
                     "SwiftMetrics",
                     "Health",
-                    "Kitura-CORS"
+                    "KituraCORS"
                 ])
     ]
 )
